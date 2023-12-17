a9432908a55b4d4511bf07feb8eb2f506ed10bc6
Angus Moore <angusmoore.dev@gmail.com>
2023-12-17 15:25:16 +1000

    style formatting for past dreams

--- components/PastDreamComponent.tsx
+++ components/PastDreamComponent.tsx
@@ -20,15 +20,15 @@ export default function PastDreamComponent({ dream, onDelete }: PastDreamCompone
     return <>
     <div className="w-full flex flex-col md:flex-row items-center gap-2 bg-[#0e0e0e] p-4 rounded transition hover:bg-[#111111] hover:cursor-pointer"
     onClick={open}>
-        <span className="text-xl text-center md:text-left font-bold md:mr-auto">
+        <span className="text-xl text-center md:text-left font-bold md:mr-auto max-w-[66%]">
             {dream.title}
         </span>
+        <small>
+            {new Date(dream.createdAt).toLocaleDateString('en-AU', { day: 'numeric', month: 'short', year: 'numeric' })}
+        </small>
         <div className="rounded-full p-1 text-sm flex items-center justify-center w-32 bg-[#141414]">
             {dream.dreamType}
         </div>
-        <small>
-            {new Date(dream.createdAt).toLocaleDateString('en-AU', { dateStyle: 'full' })}
-        </small>
     </div>
     <Modal opened={opened} onClose={close} centered size={'xl'} title={`${dream.title} | ${new Date(dream.createdAt).toLocaleDateString('en-AU', { dateStyle: 'full' })} | ${dream.dreamType}`}>
         <div className="w-full flex flex-col items-center max-w-2xl mx-auto mr-24 md:mr-12">
